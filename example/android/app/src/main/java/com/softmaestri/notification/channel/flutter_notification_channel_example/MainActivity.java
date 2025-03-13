29814f3b6e68f168e48a1bdf18d55277a5d652b9
Musa Rikhotso <musadisparta@gmail.com>
2025-03-13 11:55:55 +0200

    CHORE: Migrated example app to latest gradle structure

--- example/android/app/src/main/java/com/softmaestri/notification/channel/flutter_notification_channel_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.softmaestri.notification.channel.flutter_notification_channel_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
